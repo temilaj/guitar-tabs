f5f76efc866a2b509ffb37e38b7fd12f19344ea1
Temi Lajumoke <temi@temilajumoke.com>
2017-09-09 21:07:08 +0100

    setup server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT || 4200;
+
+app.use(morgan('combine'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello World',
+  });
+});
+
+app.listen(port, () => {
+  console.log('Hello World!');
+});
