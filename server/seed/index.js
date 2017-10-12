429a824300ab6ba6426a02e5bb65cdee68c39cd6
Temi Lajumoke <temi@temilajumoke.com>
2017-10-12 11:13:50 +0100

    add database initializers

---
+++ server/seed/index.js
@@ -0,0 +1,11 @@
+const { sequelize, Song, User } = require('../src/models');
+
+const Promise = require('bluebird');
+const songs = require('./songs.json');
+const users = require('./users.json');
+
+sequelize.sync({ force: true })
+  .then(async () => {
+    await Promise.all(users.map(user => User.create(user)));
+    await Promise.all(songs.map(song => Song.create(song)));
+  });
